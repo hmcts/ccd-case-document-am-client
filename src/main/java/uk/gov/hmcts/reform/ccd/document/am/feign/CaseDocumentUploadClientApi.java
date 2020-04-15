3bcdbaeb5ee7776c1ea206f50ab3c77e1dabd132
ConorH994 <conor.halpin@hmcts.net>
2020-04-15 12:19:19 +0100

    added upload client api

---
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/feign/CaseDocumentUploadClientApi.java
@@ -0,0 +1,27 @@
+package uk.gov.hmcts.reform.ccd.document.am.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@FeignClient(name = "case-document-am-upload-api",
+    url = "${case_document_am.url}")
+public interface CaseDocumentUploadClientApi {
+
+    @RequestMapping(method = RequestMethod.POST, value = "cases/documents")
+    ResponseEntity<Resource> uploadDocument(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
+                                            @RequestHeader("ServiceAuthorization") String serviceAuth,
+                                            @PathVariable("files") List<MultipartFile> files,
+                                            @PathVariable("classification") String classification,
+                                            @PathVariable("caseTypeId") String caseTypeId,
+                                            @PathVariable("jurisdictionId") String jurisdictionId);
+}
