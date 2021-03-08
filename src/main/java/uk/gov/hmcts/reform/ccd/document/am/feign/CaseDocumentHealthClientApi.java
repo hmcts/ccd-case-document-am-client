e2006339def3bd837cbf7dbb96091090ceac0664
tomxelliott <tomelliott@Toms-MacBook-Pro.local>
2021-03-08 16:27:31 +0000

    RDM-11101: Move health check condition to CaseDocumentManagementHealthIndicator class

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/feign/CaseDocumentHealthClientApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/feign/CaseDocumentHealthClientApi.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.ccd.document.am.feign;
 
-import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import uk.gov.hmcts.reform.ccd.document.am.healthcheck.InternalHealth;
@@ -8,7 +7,6 @@ import uk.gov.hmcts.reform.ccd.document.am.healthcheck.InternalHealth;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@ConditionalOnEnabledHealthIndicator(value = "case-document-am-api")
 @FeignClient(name = "case-document-am-health-client-api", url = "${case_document_am.url}")
 public interface CaseDocumentHealthClientApi {
 
