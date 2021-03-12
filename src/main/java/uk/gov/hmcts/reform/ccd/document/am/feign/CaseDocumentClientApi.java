3a0e3335e61f60834c137145be7e9c22579eeb65
Tom Elliott <tomxelliott@hotmail.co.uk>
2021-03-12 13:11:11 +0000

    RDM-11101: Remove /attachToCase related code

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/feign/CaseDocumentClientApi.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/feign/CaseDocumentClientApi.java
@@ -6,14 +6,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import uk.gov.hmcts.reform.ccd.document.am.model.CaseDocumentsMetadata;
 import uk.gov.hmcts.reform.ccd.document.am.model.Document;
 
 import java.util.List;
@@ -30,10 +27,6 @@ public interface CaseDocumentClientApi {
                                    @RequestParam(value = "classification") String classification,
                                    @RequestParam(value = "caseTypeId") String caseTypeId,
                                    @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuth);
-
-    @PatchMapping("/attachToCase")
-    ResponseEntity updateCaseDocumentMetadata(@RequestBody CaseDocumentsMetadata caseDocumentsMetadata,
-                                              @RequestHeader(SERVICE_AUTHORIZATION) String serviceAuth);
 
     @GetMapping(value = "/{documentId}/binary")
     ResponseEntity<Resource> getDocumentBinary(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorisation,
