780d6da84a85e09a48b3a76a6f935fefb9708a87
Kiran.Yenigala <kiran.yenigala@hmcts.net>
2026-09-15 15:36:42 +0100

    CME-1080: Implement Spring Boot 4.1.x migration across Core Case Data repositories

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/config/CaseDocumentManagementClientAutoConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/config/CaseDocumentManagementClientAutoConfiguration.java
@@ -1,15 +1,14 @@
 package uk.gov.hmcts.reform.ccd.document.am.config;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.health.autoconfigure.contributor.ConditionalOnEnabledHealthIndicator;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentHealthClientApi;
 import uk.gov.hmcts.reform.ccd.document.am.healthcheck.CaseDocumentManagementHealthIndicator;
 
-
-@Configuration
+@AutoConfiguration
 @ConditionalOnProperty(prefix = "case_document_am", name = "url")
 @EnableFeignClients(basePackages = "uk.gov.hmcts.reform.ccd.document.am")
 public class CaseDocumentManagementClientAutoConfiguration {
---
+++ src/test/java/uk/gov/hmcts/reform/ccd/document/am/config/CaseDocumentManagementClientAutoConfigurationTest.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.ccd.document.am.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentClientApi;
+import uk.gov.hmcts.reform.ccd.document.am.feign.CaseDocumentHealthClientApi;
+import uk.gov.hmcts.reform.ccd.document.am.healthcheck.CaseDocumentManagementHealthIndicator;
+import uk.gov.hmcts.reform.ccd.document.testsupport.IsolatedAutoConfigurationTestApplication;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(
+    classes = IsolatedAutoConfigurationTestApplication.class,
+    webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
+@TestPropertySource(properties = "case_document_am.url=http://localhost:5171")
+class CaseDocumentManagementClientAutoConfigurationTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void shouldLoadAutoConfigurationFromImportsFile() {
+        assertThat(Thread.currentThread().getContextClassLoader().getResource(
+            "META-INF/spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports"))
+            .as("Boot 4 auto-configuration must be registered in AutoConfiguration.imports")
+            .isNotNull();
+    }
+
+    @Test
+    void shouldRegisterFeignClientsAndHealthIndicatorWhenUrlIsSet() {
+        assertThat(context.getBean(CaseDocumentClientApi.class)).isNotNull();
+        assertThat(context.getBean(CaseDocumentHealthClientApi.class)).isNotNull();
+        assertThat(context.getBean(CaseDocumentManagementHealthIndicator.class)).isNotNull();
+    }
+}
